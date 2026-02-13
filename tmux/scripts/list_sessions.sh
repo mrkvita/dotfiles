f596573c3f8ac2a19678454a72eed889d62e362b
mrkvita <mrkvica2003@gmail.com>
2026-02-13 11:46:03 +0100

    feat: Remove shadows from picom

--- tmux/scripts/list_sessions.sh
+++ tmux/scripts/list_sessions.sh
@@ -5,9 +5,9 @@ sessions=$(tmux list-sessions -F '#S')
 
 for s in $sessions; do
   if [[ "$s" == "$current" ]]; then
-    line+="#[fg=#ea6962,bg=default,bold] $s #[fg=#707880,bg=default]:"
+    line+="#[fg=#ea6962,bg=default] $s #[fg=#707880,bg=default]:"
   else
-    line+="#[fg=#333333,bg=default] $s #[fg=#707880,bg=default]:"
+    line+="#[fg=#888888,bg=default] $s #[fg=#707880,bg=default]:"
   fi
 done
 
